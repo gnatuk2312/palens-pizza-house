90d178b1dbcf7631375bceb4ab688a19814b70ff
gnatuk2312 <gnat.liashenko@gmail.com>
2022-03-20 18:07:27 +0200

    connected styles (app.scss) to layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MainView from './pages/MainView';
 import CartView from './pages/CartView';
 import EmptyCart from './components/cartView/EmptyCart';
-import './scss/App.css';
+import './scss/app.css';
 
 function App() {
 	return (
