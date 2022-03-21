6f11a8e9972bd49a7b5f113e3bcc8c7467d5fe11
gnatuk2312 <gnat.liashenko@gmail.com>
2022-03-21 20:36:18 +0200

    make changing active class when clicked on diff filter

--- src/pages/MainView.jsx
+++ src/pages/MainView.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PizzaBlock from '../components/mainView/PizzaBlock';
 
 const MainView = () => {
+
+	const [activeClass, setActiveClass] = useState(null);
+
+	const filters = ['Піци', 'Напої', 'Десерти'];
+
 	return (
 		<div className="content">
 			<div className="container">
 				<div className="categories">
 					<ul>
-						<li className="active">Все</li>
-						<li>Піци</li>
-						<li>Напої</li>
-						<li>Десерти</li>
+						<li
+							onClick={() => setActiveClass(null)}
+							className={activeClass === null ? "active" : ""}
+						>Все</li>
+
+						{filters.map((item, index) => {
+							return (
+								<li
+									onClick={() => setActiveClass(index)}
+									key={item}
+									className={activeClass === index ? "active" : ""}
+								>{item}</li>
+							)
+						})}
 					</ul>
 				</div>
 				<div className="content__items">
