423f74c3d158a4546daca29397e48f405efb682f
gnatuk2312 <gnat.liashenko@gmail.com>
2022-03-24 15:41:59 +0200

    Connect the cart reducer to LocalStorage

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-import { createStore } from 'redux';
-import { rootReducer } from './redux/rootReducer';
+import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
-const store = createStore(rootReducer);
+
 
 ReactDOM.render(
 	<Provider store={store}>
