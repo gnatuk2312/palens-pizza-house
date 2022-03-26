b7547a73892996401ba8246d329549fc02d43558
gnatuk2312 <gnat.liashenko@gmail.com>
2022-03-26 23:36:13 +0200

    add animation of changing count of item in cart

--- src/components/cartView/CartItem.jsx
+++ src/components/cartView/CartItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { useDispatch } from 'react-redux';
 import { decrementAmount, incrementAmount, deleteItemFromCart } from '../../redux/actions/cart';
 
@@ -16,6 +17,13 @@ const CartItem = ({ item, amount }) => {
 	const increment = () => {
 		dispatch(incrementAmount(item.id))
 	};
+
+	let count = 0;
+	amount.forEach(obj => {
+		if (obj.id === item.id) {
+			count = obj.count;
+		};
+	});
 
 	return (
 		<article className="cart__item">
@@ -40,11 +48,14 @@ const CartItem = ({ item, amount }) => {
 							<path d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z" fill="#EB5A1E" />
 						</svg>
 					</div>
-					{amount.map(elem => {
-						if (elem.id === item.id) {
-							return (<b key={elem.id}>{elem.count}</b>);
-						}; return null;
-					})}
+					<TransitionGroup component='span' className='count'>
+						<CSSTransition
+							classNames='count'
+							key={count}
+							timeout={400}>
+							<div>{count}</div>
+						</CSSTransition>
+					</TransitionGroup>
 					<div onClick={increment} className="button button--outline button--circle cart__item-count-plus">
 						<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
 							<path d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z" fill="#EB5A1E" />
