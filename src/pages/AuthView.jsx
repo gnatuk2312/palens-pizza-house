883fa4ae85436be1439092835c7016326a0cfc4c
gnatuk2312 <gnat.liashenko@gmail.com>
2022-04-01 22:48:58 +0300

    fix bug in AuthView (<a/> without href={}) rewrite into to <button/>

--- src/pages/AuthView.jsx
+++ src/pages/AuthView.jsx
@@ -14,12 +14,12 @@ const AuthView = () => {
 			<h3 className='auth-title'>Для того щоб перейти у корзину або зробити замовлення увійдіть у свій особистий кабінет</h3>
 			<article className="auth-register">
 				<nav className='auth-buttons' >
-					<a
+					<button
 						className={currentForm === 'Вхід' ? 'auth-button active' : 'auth-button'}
-						onClick={handleClick} >Вхід</a>
-					<a
+						onClick={handleClick} >Вхід</button>
+					<button
 						className={currentForm === 'Реєстрація' ? 'auth-button active' : 'auth-button'}
-						onClick={handleClick} >Реєстрація</a>
+						onClick={handleClick} >Реєстрація</button>
 				</nav>
 				{currentForm === 'Вхід' ? <Authorisation /> : <Register />}
 				<div className='auth-bottom'></div>
