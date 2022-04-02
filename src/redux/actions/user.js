8db20de153a1ac595aa09ee544fdf14d2e9e9a91
gnatuk2312 <gnat.liashenko@gmail.com>
2022-04-02 19:55:13 +0300

    handle errors in switch case by error-code

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -9,25 +9,44 @@ export const registerUser = (name, surname, login, password) => {
 				"name": name,
 				"surname": surname,
 				"password": password
+			}).catch(e => {
+				throw e;
 			});
 
-			switch (response.status) {
-				case 201:
-					dispatch({
-						type: SET_USER,
-						payload: response.data
-					});
-					console.log('Succesful REGISTER');
+			if (response.status === 201) {
+				dispatch({
+					type: SET_USER,
+					payload: response.data
+				});
+				console.log('Succesful REGISTER');
+			}
+
+			console.log(response);
+
+		} catch (err) {
+			const error = Number(err.message.substring(err.message.length - 3, err.message.length));
+			console.log(error);
+
+			switch (error) {
+				case 400:
+					console.log('Bad Request');
+					break;
+
+				case 404:
+					console.log('Bad path in request');
+					break;
+
+				case 404:
+					console.log('Login in use');
+					break;
+
+				case 500:
+					console.log('problems in server');
 					break;
 
 				default:
 					break;
 			};
-
-			console.log(response);
-
-		} catch (err) {
-			console.log('error while REGISTER >>', err);
 		}
 	};
 };
@@ -38,25 +57,44 @@ export const loginUser = (login, password) => {
 			const response = await axios.post('https://whispering-river-85355.herokuapp.com/api/users/signin', {
 				"login": login,
 				"password": password
+			}).catch(e => {
+				throw e;
 			});
 
-			switch (response.status) {
-				case 200:
-					dispatch({
-						type: SET_USER,
-						payload: response.data
-					});
-					console.log('Succesful LOGIN');
-					break;
-
-				default:
-					break;
+			if (response.status === 200) {
+				dispatch({
+					type: SET_USER,
+					payload: response.data
+				});
+				console.log('Succesful LOGIN');
 			};
 
 			console.log(response);
 
 		} catch (err) {
-			console.log('error while LOGIN >>', err);
+			const error = Number(err.message.substring(err.message.length - 3, err.message.length));
+			console.log(error);
+
+			switch (error) {
+				case 400:
+					console.log('Bad Request');
+					break;
+
+				case 401:
+					console.log('Wrong Password or login');
+					break;
+
+				case 404:
+					console.log('Bad path in request');
+					break;
+
+				case 500:
+					console.log('problems in server');
+					break;
+
+				default:
+					break;
+			};
 		}
 	};
 };
